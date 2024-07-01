cf9c326d04d1c899ff55cb4be0490d90b84aa5f7
BarbaraRM <bromeromachare@gmail.com>
2024-07-01 04:04:08 -0700

    dashboard and new form

---
+++ js/dashboard.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = '6844ef2390msh7e333101398ad58p1a29a0jsn19bb6d312a99';
+    const symbols = ['AG:NYSE', 'NEM:NYSE', 'RGLD:NASDAQ'];
+    const period = 'QUARTERLY';
+    const language = 'en';
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey,
+            'x-rapidapi-host': 'real-time-finance-data.p.rapidapi.com'
+        }
+    };
+
+    async function fetchData(symbol, tableId) {
+        const endpoint = `https://real-time-finance-data.p.rapidapi.com/company-balance-sheet?symbol=${symbol}&period=${period}&language=${language}`;
+
+        try {
+            const response = await fetch(endpoint, options);
+            const result = await response.json();
+            displayData(result.data.balance_sheet, tableId);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    function displayData(balanceSheet, tableId) {
+        const tableBody = document.getElementById(tableId);
+        tableBody.innerHTML = '';
+
+        balanceSheet.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <th scope="row">${item.year}</th>
+                <td>$${item.cash_and_short_term_investments}</td>
+                <td>${item.return_on_assets_percent}%</td>
+                <td>${item.return_on_capital_percent}%</td>
+            `;
+            tableBody.appendChild(row);
+        });
+    }
+
+    fetchData(symbols[0], 'table-body-1');
+    fetchData(symbols[1], 'table-body-2');
+    fetchData(symbols[2], 'table-body-3');
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const logoutLink = document.getElementById('logout');
+    
+    logoutLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevenir comportamiento predeterminado del enlace
+        localStorage.removeItem('user'); // Eliminar el token del usuario del localStorage
+        // Redirigir a la página de inicio o a una página de login
+        window.location.href = '/pages/login.html';
+    });
+});
